7fc43d9358b8df477c0bd47d017336362a5e4358
Dmitry Obryvkov <dmitry.obryvkov213@mail.ru>
2022-10-13 00:31:21 +0300

    Подключает js  модуль к html, производит разбивку js кода на модули

---
+++ js/utils.js
@@ -0,0 +1,15 @@
+const getRandomInt = (min, max) => {
+  if ((max < 0) || (min < 0)) {
+    return NaN;
+  }
+  min = Math.ceil(Math.min(min, max));
+  max = Math.floor(Math.max(min, max));
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const getCorrectLength = (text, maxLenght) => text.length <= maxLenght;
+getCorrectLength();
+
+const getRandomElementArray = (array) => array[getRandomInt(0, array.length - 1)];
+
+export {getRandomInt, getRandomElementArray};

2faf0ca27a345f7b65078b446d2efab9482962b0
Dmitry Obryvkov <dmitry.obryvkov213@mail.ru>
2022-10-22 20:34:37 +0300

    Добавляет и отображает миниатюры

--- js/utils.js
+++ js/utils.js
@@ -7,9 +7,12 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+/*
 const getCorrectLength = (text, maxLenght) => text.length <= maxLenght;
 getCorrectLength();
-
+Закомментировал потому, что из-за этого ругался браузер и не хотел отображать код из thumbnails.js
+Ругался из-за того, что не найден text
+*/
 const getRandomElementArray = (array) => array[getRandomInt(0, array.length - 1)];
 
 export {getRandomInt, getRandomElementArray};
